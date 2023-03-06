4792ab1713d74051c171ab44775db4c3ce9e7318
Langston Barrett <langston.barrett@gmail.com>
2023-03-06 11:23:39 -0500

    Handle duplicate facts

--- src/eval.rs
+++ src/eval.rs
@@ -1,6 +1,6 @@
 use duckdb::{Connection, Result};
 
-use crate::ast::{Atom, Program, Rel};
+use crate::ast::{GroundAtom, Program, Rel};
 
 #[derive(Debug)]
 pub struct Eval {
@@ -38,25 +38,45 @@ fn create_tables(conn: &Connection, prog: &Program) -> Result<()> {
     Ok(())
 }
 
-// fn exists(fact: &Atom) -> Result<bool> {
-//     let mut entries = conn
-//         .prepare_cached(&format!("SELECT COUNT(*) from {} WHERE;", fact.rel))
-//         .unwrap();
-// }
+fn exists(conn: &Connection, fact: &GroundAtom) -> Result<bool> {
+    let mut q = format!("SELECT COUNT(*) from {}", fact.rel);
+    if !fact.terms.is_empty() {
+        q += " WHERE ";
+        for (i, term) in fact.terms.iter().enumerate() {
+            q += &format!("{}.x{} = '{}'", fact.rel, i, term);
+        }
+    }
+    q += ";";
 
-// TODO(lb, high): Handle duplicate facts
+    let mut entries = conn.prepare_cached(&q).unwrap();
+    let n: usize = entries
+        .query([])?
+        .next()?
+        .expect("No rows for COUNT?")
+        .get(0)?;
+    debug_assert!(n == 0 || n == 1);
+    Ok(n >= 1)
+}
+
 // TODO(lb, low): Group facts by relation, use Appender
-fn insert_fact(conn: &Connection, fact: &Atom) -> Result<()> {
+fn insert_fact(conn: &Connection, fact: &GroundAtom) -> Result<()> {
     let mut q = format!(r"INSERT INTO {0} VALUES (nextval('{0}_seq')", fact.rel);
     for _ in &fact.terms {
         q += ", ?";
     }
     q += ");";
+
     let mut stmt = conn.prepare_cached(&q)?;
-    let ground = fact.clone().ground().expect("Range restriction violation!");
-    stmt.execute(duckdb::params_from_iter(ground.terms))?;
+    stmt.execute(duckdb::params_from_iter(&fact.terms))?;
     conn.flush_prepared_statement_cache();
     Ok(())
+}
+
+fn insert_fact_if_not_exists(conn: &Connection, fact: &GroundAtom) -> Result<()> {
+    if exists(conn, fact)? {
+        return Ok(());
+    }
+    insert_fact(conn, fact)
 }
 
 fn insert_facts(conn: &Connection, prog: &Program) -> Result<()> {
@@ -64,7 +84,12 @@ fn insert_facts(conn: &Connection, prog: &Program) -> Result<()> {
     conn.set_prepared_statement_cache_capacity(512); // just a guess
     for rule in &prog.rules {
         if rule.is_fact() {
-            insert_fact(conn, &rule.head)?;
+            let ground = rule
+                .head
+                .clone()
+                .ground()
+                .expect("Range restriction violation!");
+            insert_fact_if_not_exists(conn, &ground)?;
         }
     }
     conn.execute_batch("COMMIT;")?;
@@ -133,7 +158,6 @@ mod tests {
         let eval = Eval::new(conn, prog).unwrap();
         let conn = eval.into_connection();
         let mut entries = conn.prepare("SELECT COUNT(*) from r;").unwrap();
-        // TODO: Should be 1!
         let n: usize = entries
             .query([])
             .unwrap()
@@ -141,6 +165,6 @@ mod tests {
             .unwrap()
             .unwrap()
             .get_unwrap(0);
-        assert_eq!(2, n);
+        assert_eq!(1, n);
     }
 }
