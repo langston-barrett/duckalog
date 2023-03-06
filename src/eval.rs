e3ece1137f13f44690e6867296f2b5a9dfe7e1db
Langston Barrett <langston.barrett@gmail.com>
2023-03-06 10:32:54 -0500

    Remove DuckDB README code

--- src/eval.rs
+++ src/eval.rs
@@ -80,47 +80,6 @@ impl Eval {
     }
 }
 
-// fn main() -> Result<()> {
-//     let conn = Connection::open_in_memory()?;
-
-//     conn.execute_batch(
-//         r"CREATE SEQUENCE seq;
-//           CREATE TABLE person (
-//                   id              INTEGER PRIMARY KEY DEFAULT NEXTVAL('seq'),
-//                   name            TEXT NOT NULL,
-//                   data            BLOB
-//                   );
-//          ",
-//     )?;
-//     let me = Person {
-//         id: 0,
-//         name: "Steven".to_string(),
-//         data: None,
-//     };
-//     conn.execute(
-//         "INSERT INTO person (name, data) VALUES (?, ?)",
-//         params![me.name, me.data],
-//     )?;
-
-//     let mut stmt = conn.prepare("SELECT id, name, data FROM person")?;
-//     let person_iter = stmt.query_map([], |row| {
-//         Ok(Person {
-//             id: row.get(0)?,
-//             name: row.get(1)?,
-//             data: row.get(2)?,
-//         })
-//     })?;
-
-//     for person in person_iter {
-//         println!("Found person {:?}", person.unwrap());
-//     }
-
-//     // query table by arrow
-//     let rbs: Vec<RecordBatch> = stmt.query_arrow([])?.collect();
-//     print_batches(&rbs);
-//     Ok(())
-// }
-
 #[cfg(test)]
 mod tests {
     use super::*;
